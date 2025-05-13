1fedf59feb29af1de4c36170cd5d1a0a267b769e
Kyaw Khine Htoo <alberthtoo.k@gmail.com>
2025-05-13 21:29:30 +0700

    reverb fix

--- routes/console.php
+++ routes/console.php
@@ -3,13 +3,18 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\CustomerController;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-// Define and schedule the command directly
-Artisan::command('customers:expire-to-suspend')->daily()->purpose('Change status of expired customers to suspended');
+// Schedule existing commands
+Schedule::command('customers:expire-to-suspend')
+    ->daily()
+    ->appendOutputTo(storage_path('logs/customers-expire-suspend.log'));
 
 // Schedule the radius:import command to run at 3:30 PM daily
-Artisan::command('radius:import')->dailyAt('15:30')->purpose('Import radius users from radius server');
+Schedule::command('radius:import')
+    ->dailyAt('15:30')
+    ->appendOutputTo(storage_path('logs/radius-import.log'));
